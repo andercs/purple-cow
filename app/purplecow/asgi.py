6d7c70cc89ff9f43e43723c93608f76f8ccb1cbd
Connor Anderson <connoranderson0418@gmail.com>
2021-02-27 21:39:59 -0500

    [#3] Add developer tooling

--- app/purplecow/asgi.py
+++ app/purplecow/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'purplecow.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "purplecow.settings")
 
 application = get_asgi_application()
