7def5294cd3d27d3785e94e8f1e6ed5bf1c1387d
Connor Anderson <connoranderson0418@gmail.com>
2021-02-28 22:58:00 -0500

    [#12] Make webapp port more configurable

--- startup.sh
+++ startup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+if [[ ! -f .env ]]; then
+  cp .docker/.env.template ./.env
+fi
+
 if [[ "$(docker images -q purple-cow_db:latest 2> /dev/null)" == "" ]]; then
-  docker-compose build
+  docker-compose build -f .docker/docker-compose.yml
 else
   docker build -t purple-cow_db -f .docker/db/Dockerfile .
   docker build -t purple-cow_webapp -f .docker/webapp/Dockerfile .
 fi
 
-docker-compose up -d
+docker-compose -f .docker/docker-compose.yml up -d
